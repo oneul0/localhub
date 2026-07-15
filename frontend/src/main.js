9c4d054446e7a736770b249f83a1b8004d67ca14
oneul0 <neul.dev@gmail.com>
2026-07-15 14:50:58 +0900

    fix: 배포 환경에서 leaflet 핀 이미지 불러오지 못하는 오류 해결

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,31 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import L from 'leaflet'
+
 import App from './App.vue'
 import router from './router'
+
 import './style.css'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+// Leaflet이 기본 상대경로로 이미지를 찾지 않도록 제거
+delete L.Icon.Default.prototype._getIconUrl
+
+// Vite가 빌드한 실제 이미지 경로 사용
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+})
+
 const app = createApp(App)
+
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
